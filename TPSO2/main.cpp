7379d181d5cc4c2847406d51b341aadef0818da1
Temennigru <temennigru@github.com>
2014-10-28 12:14:22 -0200

    Initial commit

---
+++ TPSO2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TPSO2
+//
+//  Created by Jean-Luc Nacif Coelho on 10/28/14.
+//  Copyright (c) 2014 GrupoSO. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
